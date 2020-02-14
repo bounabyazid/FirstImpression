9c8c7b1fe57a976e7c2e740036238b020beaa311
Yazid Bounab <bounab.yazid@gmail.com>
2020-02-14 15:28:18 +0200

    Add files via upload

--- Train_DL_WE.py
+++ Train_DL_WE.py
@@ -20,11 +20,10 @@ from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
 from PreprocesingTranscriptions import Preprocessing_Dataset
 
 import matplotlib.pyplot as plt
+
 plt.style.use('ggplot')
 
 def LoadLabels(Prediction, Annotation, keys):
@@ -69,43 +68,67 @@ def LoadDataSet():
     X_val = pad_sequences(X_val, padding='post', maxlen=maxlen)
     X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
 
-    return  X_train, X_val, X_test, Y_train, Y_val, Y_test
+    return  X_train, X_val, X_test, Y_train, Y_val, Y_test, vocab_size
    
 def KerasDeeepLearning():
-    X_train, X_val, X_test, Y_train, Y_val, Y_test = LoadDataSet()
+    X_train, X_val, X_test, Y_train, Y_val, Y_test, vocab_size = LoadDataSet()
+    maxlen = 100
+
+    model = Sequential()
+
+    embedding_dim = 100
+
+    model = Sequential()
+    model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(10, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+    model.summary()
+
+    History = model.fit(X_train, Y_train, epochs=50, verbose=False, 
+                    validation_data=(X_val, Y_val), batch_size=10)
     
-    input_dim = X_train.shape[1]  # Number of features
-
-    MODEL = Sequential()
-#    MODEL.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
-#    MODEL.add(layers.Dense(1, activation='sigmoid'))
-
-    MODEL.add(layers.Dense(40, input_dim=input_dim, activation='relu'))
-    MODEL.add(layers.Dense(15, activation='relu'))
-    MODEL.add(layers.Dense(1, activation='sigmoid'))
-
-#    MODEL.add(layers.Dense(30, input_dim=input_dim, activation='relu'))
-#    MODEL.add(layers.Dense(15, activation='relu'))
-#    MODEL.add(layers.Dense(7, activation='relu'))
-#    MODEL.add(layers.Dense(1, activation='sigmoid'))
-
-
-    MODEL.compile(loss='mse', optimizer='adam', metrics=['mae'])
-    MODEL.summary()
-
-    History = MODEL.fit(X_train, Y_train, epochs=25, verbose=False, 
-                        validation_data=(X_val, Y_val), batch_size=30)
-    
-    loss, MEA = MODEL.evaluate(X_train, Y_train, verbose=False)
+    loss, MEA = model.evaluate(X_train, Y_train, verbose=False)
     print("Training MAE: {:.4f} Correct {:.4f}".format(MEA,1-MEA))
     
-    loss, MEA = MODEL.evaluate(X_test, Y_test, verbose=False)
+    loss, MEA = model.evaluate(X_test, Y_test, verbose=False)
     print("Testing MAE:  {:.4f} Correct {:.4f}".format(MEA,1-MEA))
 
     return History
 
-#    filename = 'Interview_DL_model.sav'
-#    pickle.dump(MODEL, open(filename, 'wb'))
+def KerasWordEmbedding():
+    X_train, X_val, X_test, Y_train, Y_val, Y_test, vocab_size = LoadDataSet()
+    maxlen = 100
+
+    model = Sequential()
+
+    embedding_dim = 100
+
+    model = Sequential()
+    model.add(layers.Embedding(input_dim=vocab_size,
+                               output_dim=embedding_dim, 
+                               input_length=maxlen))
+    
+    model.add(layers.GlobalMaxPool1D())
+    model.add(layers.Dense(10, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+    model.summary()
+
+    History = model.fit(X_train, Y_train, epochs=50, verbose=False, 
+                    validation_data=(X_val, Y_val), batch_size=10)
+    
+    loss, MEA = model.evaluate(X_train, Y_train, verbose=False)
+    print("Training MAE: {:.4f} Correct {:.4f}".format(MEA,1-MEA))
+    
+    loss, MEA = model.evaluate(X_test, Y_test, verbose=False)
+    print("Testing MAE:  {:.4f} Correct {:.4f}".format(MEA,1-MEA))
+
+    return History
+
 
 def plot_history(History):
     mae = History.history['mean_absolute_error']
@@ -126,21 +149,6 @@ def plot_history(History):
     plt.title('Training and validation loss')
     plt.legend()
     
-    
-def LoadBaseLineModel(X_test, Y_test):
-    filename = 'Interview_DL_model.sav'
-    MODEL = pickle.load(open(filename, 'rb'))
-    YP_test = MODEL.predict(X_test, Y_test)
-    
-    mae_ts = round(1 - mean_absolute_error(Y_test, YP_test),4)
-    mse_ts = round(1 - mean_squared_error(Y_test, YP_test),4)
-    r2_ts = round(r2_score(Y_test, YP_test),4)
-
-    print('MAE TS = ' + str(mae_ts))
-    print('........................')
-    print('MSE TS = ' + str(mse_ts))
-    print('........................')
-    print('R2 TS = ' + str(r2_ts))
-
 History = KerasDeeepLearning()
+#History = KerasWordEmbedding()
 plot_history(History)
